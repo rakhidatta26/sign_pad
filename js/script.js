a3a566b0bed2675c1b971381f0f3e3ab6b4fe2ad
Rakhi Datta <146545970+rakhidatta26@users.noreply.github.com>
2023-12-28 16:49:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $('#bcPaint').bcPaint({
+    // default color
+    defaultColor : '000000',
+
+    // default color set
+    colors : [
+      '000000', '444444', '999999', 'DDDDDD', '6B0100', 'AD0200',
+      '6B5E00', 'FFE000', '007A22', '00E53F', '000884', '000FFF'
+    ],
+
+    // extend default set
+    addColors : []
+  });     
+});
